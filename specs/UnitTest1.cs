31e459f43dbf4961128d75c91b723e6b336caacd
i-will-lie <i-will-lie@users.noreply.github.com>
2020-09-20 19:50:52 +1200

    Initial commit

---
+++ specs/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using ContosoUniversity.
+
+namespace ContosoUniversity.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
